03e7d8ad2373141898f2428fff8d7094e2859c95
Hermenegildo <Hermenegildo@Hermenegildo-PC>
2011-04-16 14:36:46 -0300

    ultima atuacao

--- application/dao/Dao.php
+++ application/dao/Dao.php
@@ -1,93 +1,125 @@
 <?php
-namespace login\application\dao {
-	use PDO;
-	class Dao {
-		///////////////////////////////////////////////////////////////////
-		/////////////////////ATRIBUTOS PRINCIPAIS//////////////////////////
-		///////////////////////////////////////////////////////////////////
-		protected $conn = null;
-		private $host = 'localhost';
-		private $banco = 'login';
-		private $usuario = 'root';
-		private $senha = '';
-		///////////////////////////////////////////////////////////////////
-		//////////////////////METODOS PRINCIPAIS///////////////////////////
-		///////////////////////////////////////////////////////////////////
-		
-		public function __construct () {
-			$this->connect();
-			$this->exportToTxt();
-			$this->exportToXML();
-		}
-		
-		///////////////////////////////////////////////////////////////////
-		//////////////////////METODOS ALXILIARES///////////////////////////
-		///////////////////////////////////////////////////////////////////
-		
-		private function connect () {
-			$this->conn = new PDO('mysql:host=localhost;dbname=login', 'root', '');
-			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		}
-		
-		private function exportToTxt () {
-			$pathTxt = _PATH_FILES_ . 'txt/';
-			if (!is_dir($pathTxt))
-				mkdir($pathTxt, 0777, true);
-				
-			$usuarios = fopen($pathTxt . 'user.txt', 'w');
-			$queryUsuarios = 'SELECT * FROM user ORDER BY id';
-			$u = $this->conn->query($queryUsuarios)->fetchAll();
-			if ($u) {
-				foreach ($u as $arrayUsers) {
-					fwrite($usuarios, $arrayUsers['id'] . ';' . $arrayUsers['login'] . ';' . $arrayUsers['password'] . ';' . $arrayUsers['type'] . "\n");
-				}
-			}
-			fclose($usuarios);
+class Dao {
+	///////////////////////////////////////////////////////////////////
+	/////////////////////ATRIBUTOS PRINCIPAIS//////////////////////////
+	///////////////////////////////////////////////////////////////////
+	protected $conn = null;
+	private $host = 'localhost';
+	private $banco = 'login';
+	private $usuario = 'root';
+	private $senha = '';
+	///////////////////////////////////////////////////////////////////
+	//////////////////////METODOS PRINCIPAIS///////////////////////////
+	///////////////////////////////////////////////////////////////////
+	
+	public function __construct () {
+		$this->connect();
+		$this->exportToTxt();
+		$this->exportToXML();
+	}
+	
+	///////////////////////////////////////////////////////////////////
+	//////////////////////METODOS ALXILIARES///////////////////////////
+	///////////////////////////////////////////////////////////////////
+	
+	private function connect () {
+		$this->conn = new PDO('mysql:host=localhost;dbname=login', 'root', '');
+		$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	
+	private function connectOutlet () {
+		return array(
+		  'connection' => array(
+		    'dsn'      => 'mysql:host=locahost;dbname=login',
+		    'username' => 'root',
+		    'password' => '',
+		    'dialect'  => 'mysql'
+		  ),
+		  'classes' => array(
+		    'Project' => array(
+		      'table' => 'projects',
+		      'props' => array(
+		        'ID'        => array('id', 'int', array('pk'=>true, 'autoIncrement'=>true)),
+		        'Name'      => array('name', 'varchar')
+		      ),
+		      'associations' => array(
+		        array('one-to-many', 'Bug', array('key'=>'ProjectID'))
+		      )
+		    ),
+		    'Bug' => array(
+		      'table' => 'bugs',
+		      'props' => array(
+		        'ID'        => array('id', 'int', array('pk'=>true, 'autoIncrement'=>true)),
+		        'Title'     => array('title', 'varchar'),
+		        'ProjectID' => array('project_id', 'int'),
+		        'Description' => array('description', 'varchar')
+		      ),
+		      'associations' => array(
+		        array('many-to-one', 'Project', array('key'=>'ProjectID'))
+		      )
+		    )
+		  )
+		);
+	}
+	
+	private function exportToTxt () {
+		$pathTxt = _PATH_FILES_ . 'txt/';
+		if (!is_dir($pathTxt))
+			mkdir($pathTxt, 0777, true);
 			
-			$acessos = fopen($pathTxt . 'access.txt', 'w');
-			$queryAcessos = 'SELECT * FROM access ORDER BY id';
-			$u = $this->conn->query($queryAcessos)->fetchAll();
-			if ($u) {
-				foreach ($u as $arrayAcessos) {
-					fwrite($acessos, $arrayAcessos['id'] . ';' . $arrayAcessos['type'] . ';' . $arrayAcessos['datehour'] . ';' . $arrayAcessos['user_id'] . "\n");
-				}
+		$usuarios = fopen($pathTxt . 'user.txt', 'w');
+		$queryUsuarios = 'SELECT * FROM user ORDER BY id';
+		$u = $this->conn->query($queryUsuarios)->fetchAll();
+		if ($u) {
+			foreach ($u as $arrayUsers) {
+				fwrite($usuarios, $arrayUsers['id'] . ';' . $arrayUsers['login'] . ';' . $arrayUsers['password'] . ';' . $arrayUsers['type'] . "\n");
 			}
-			fclose($acessos);
-			
 		}
+		fclose($usuarios);
 		
-		private function exportToXML () {
-			$pathXml = _PATH_FILES_ . 'xml/';
-			if (!is_dir($pathXml))
-				mkdir($pathXml, 0777, true);
-				
-			$xmlUsuario = fopen($pathXml . 'user.xml', 'w');
-			$querySetores = 'SELECT * FROM user ORDER BY id';
-			$usuarios = $this->conn->query($querySetores)->fetchAll();
-			$arquivo = '<?xml version="1.0" encoding="UTF-8"?>';
-			$arquivo .= '<users>';
-			if ($usuarios) {
-				foreach ($usuarios as $usuario) {
-					$arquivo .= '<user id="' . $usuario['id'] . '" login="' . utf8_encode($usuario['login']) . '" password="' . $usuario['password'] . '" type="' . $usuario['type'] . '" />';
-				}
+		$acessos = fopen($pathTxt . 'access.txt', 'w');
+		$queryAcessos = 'SELECT * FROM access ORDER BY id';
+		$u = $this->conn->query($queryAcessos)->fetchAll();
+		if ($u) {
+			foreach ($u as $arrayAcessos) {
+				fwrite($acessos, $arrayAcessos['id'] . ';' . $arrayAcessos['type'] . ';' . $arrayAcessos['datehour'] . ';' . $arrayAcessos['user_id'] . "\n");
 			}
-			$arquivo .= '</users>';
-			$escrevendoXml = fwrite($xmlUsuario, $arquivo);
-			$xml = fclose($xmlUsuario);
-			
-			$xmlAcesso = fopen($pathXml . 'access.xml', 'w');
-			$querySetores = 'SELECT * FROM access ORDER BY id';
-			$acessos = $this->conn->query($querySetores)->fetchAll();
-			$arquivo = '<?xml version="1.0" encoding="UTF-8"?>';
-			$arquivo .= '<accesses>';
-			if ($acessos) {
-				foreach ($acessos as $acesso) {
-					$arquivo .= '<access id="' . $acesso['id'] . '" type="' . $acesso['type'] . '" datehour="' . $acesso['datehour'] . '" user_id="' . $acesso['user_id'] . '" />';
-				}
-			}
-			$arquivo .= '</accesses>';
-			$escrevendoXml = fwrite($xmlAcesso, $arquivo);
-			$xml = fclose($xmlAcesso);
 		}
+		fclose($acessos);
+		
+	}
+	
+	private function exportToXML () {
+		$pathXml = _PATH_FILES_ . 'xml/';
+		if (!is_dir($pathXml))
+			mkdir($pathXml, 0777, true);
+			
+		$xmlUsuario = fopen($pathXml . 'user.xml', 'w');
+		$querySetores = 'SELECT * FROM user ORDER BY id';
+		$usuarios = $this->conn->query($querySetores)->fetchAll();
+		$arquivo = '<?xml version="1.0" encoding="UTF-8"?>';
+		$arquivo .= '<users>';
+		if ($usuarios) {
+			foreach ($usuarios as $usuario) {
+				$arquivo .= '<user id="' . $usuario['id'] . '" login="' . utf8_encode($usuario['login']) . '" password="' . $usuario['password'] . '" type="' . $usuario['type'] . '" />';
+			}
+		}
+		$arquivo .= '</users>';
+		$escrevendoXml = fwrite($xmlUsuario, $arquivo);
+		$xml = fclose($xmlUsuario);
+		
+		$xmlAcesso = fopen($pathXml . 'access.xml', 'w');
+		$querySetores = 'SELECT * FROM access ORDER BY id';
+		$acessos = $this->conn->query($querySetores)->fetchAll();
+		$arquivo = '<?xml version="1.0" encoding="UTF-8"?>';
+		$arquivo .= '<accesses>';
+		if ($acessos) {
+			foreach ($acessos as $acesso) {
+				$arquivo .= '<access id="' . $acesso['id'] . '" type="' . $acesso['type'] . '" datehour="' . $acesso['datehour'] . '" user_id="' . $acesso['user_id'] . '" />';
+			}
+		}
+		$arquivo .= '</accesses>';
+		$escrevendoXml = fwrite($xmlAcesso, $arquivo);
+		$xml = fclose($xmlAcesso);
 	}
 }
--- application/entities/Pessoa.php
+++ application/entities/Pessoa.php
@@ -1,57 +1,54 @@
 <?php
-namespace login\application\entities {
-	use login\application\entities\User;
-	class Pessoa {
-		private $id = null;
-		private $dataCadastro = null;
-		private $situacao = null;
-		
-		/**
-		 * Atribui um valor ao 'id' da pessoa
-		 * @param int $newId Identificador da pessoa
-		 */
-		public function setId ($newId) {
-			$this->id = $newId;
-		}
-		
-		/**
-		 * Atribui um valor ao tipo do acesso
-		 * @param string $newDataCadastro Data de cadastro da pessoa
-		 */
-		public function setDataCadatro (DateTime $newDataCadastro) {
-			$this->dataCadastro = $newDataCadastro;
-		}
-		
-		/**
-		 * Atribui a situacao atual da pessoa
-		 * @param string $newSituacao E a situacao atual da pessoa ('ativa' ou 'inativa')
-		 */
-		public function setSituacao ($newSituacao) {
-			$this->situacao = $newSituacao;
-		}
-		
-		/**
-		 * Retorna o 'id' do acesso
-		 * @return int $id Identificador da pessoa
-		 */
-		public function getId () {
-			return $this->id;
-		}
-		
-		/**
-		 * Retorna a data de cadatro da pessoa
-		 * @return DateTime
-		 */
-		public function getDataCadastro () {
-			return $this->dataCadastro;
-		}
-		
-		/**
-		 * Retorna a situacao da pessoa (ativa ou inativa)
-		 * @return DateTime $dateHour Data e hora do acesso
-		 */
-		public function getSituacao () {
-			return $this->situacao;
-		}
+class Pessoa {
+	private $id = null;
+	private $dataCadastro = null;
+	private $situacao = null;
+
+	/**
+	 * Atribui um valor ao 'id' da pessoa
+	 * @param int $newId Identificador da pessoa
+	 */
+	public function setId ($newId) {
+		$this->id = $newId;
+	}
+
+	/**
+	 * Atribui um valor ao tipo do acesso
+	 * @param string $newDataCadastro Data de cadastro da pessoa
+	 */
+	public function setDataCadatro (DateTime $newDataCadastro) {
+		$this->dataCadastro = $newDataCadastro;
+	}
+
+	/**
+	 * Atribui a situacao atual da pessoa
+	 * @param string $newSituacao E a situacao atual da pessoa ('ativa' ou 'inativa')
+	 */
+	public function setSituacao ($newSituacao) {
+		$this->situacao = $newSituacao;
+	}
+
+	/**
+	 * Retorna o 'id' do acesso
+	 * @return int $id Identificador da pessoa
+	 */
+	public function getId () {
+		return $this->id;
+	}
+
+	/**
+	 * Retorna a data de cadatro da pessoa
+	 * @return DateTime
+	 */
+	public function getDataCadastro () {
+		return $this->dataCadastro;
+	}
+
+	/**
+	 * Retorna a situacao da pessoa (ativa ou inativa)
+	 * @return DateTime $dateHour Data e hora do acesso
+	 */
+	public function getSituacao () {
+		return $this->situacao;
 	}
 }
