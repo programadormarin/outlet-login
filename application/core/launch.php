1bc39a1122c5756aa49ca03a184cbef77b222f90
Hermenegildo <Hermenegildo@Hermenegildo-PC>
2011-04-17 10:35:14 -0300

    ultimo

--- application/core/launch.php
+++ application/core/launch.php
@@ -3,7 +3,7 @@ date_default_timezone_set('America/Sao_Paulo');
 
 require 'application/br.com.lcobucci.action-mapper/autoloader/ActionMapperAutoLoader.php';
 require 'application/br.com.lcobucci.display-objects/autoloader/DisplayObjectsAutoloader.php';
-require 'application/br.com.atlasdoesportesc.atlas/core/AtlasScAutoloader.php';
+require 'application/core/LoginAutoloader.php';
 require 'application/org.outlet-orm/autoloader/OutletAutoloader.php';
 
 ActionMapperAutoLoader::register();
